f246b6894775139d5a1d11eaabdaf64c057cde09
Brendan Walker <afbmwalker@gmail.com>
2019-11-10 23:21:25 -0600

    Added a few objects and scripts

---
+++ CS347 Major Project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Vector3 currPos;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currPos = transform.position;
+        if (Input.GetKey("w") || Input.GetKey("up"))
+        {
+            currPos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.GetKey("down"))
+        {
+            currPos.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            currPos.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            currPos.x += speed * Time.deltaTime;
+        }
+
+        transform.position = currPos;
+    }
+}
