71cb5f8d6ee3ef513cfc0444f9b653019d32d7e9
Brendan Walker <afbmwalker@gmail.com>
2019-11-19 13:21:29 -0600

    Camera Zoom in / out

--- CS347 Major Project/Assets/Scripts/CameraScript.cs
+++ CS347 Major Project/Assets/Scripts/CameraScript.cs
@@ -6,6 +6,7 @@ public class CameraScript : MonoBehaviour
 {
     public GameObject player;
     Vector3 offset;
+    bool zoom = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,16 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey("i") && zoom == true)
+        {
+            zoom = false;
+            offset.z = offset.z + 5;
+        }
+        if (Input.GetKey("o") && zoom == false)
+        {
+            zoom = true;
+            offset.z = offset.z - 5;
+        }
         transform.position = player.transform.position + offset;
     }
 }
