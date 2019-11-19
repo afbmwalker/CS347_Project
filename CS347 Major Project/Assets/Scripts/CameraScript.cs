9a016dfa52e469d973f1bb0babd347d1d85dbcdd
Brendan Walker <afbmwalker@gmail.com>
2019-11-18 21:32:21 -0600

    Added a zoom to camera
    press i for first person
    press o for third person

--- CS347 Major Project/Assets/Scripts/CameraScript.cs
+++ CS347 Major Project/Assets/Scripts/CameraScript.cs
@@ -6,6 +6,7 @@ public class CameraScript : MonoBehaviour
 {
     public GameObject player;
     Vector3 offset;
+    bool zoom = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,16 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey("i") && zoom == true)
+        { // Move Camera In or Zoom In
+            zoom = false;
+            offset.z = offset.z + 5.0f;
+        }
+        if (Input.GetKey("o") && zoom == false)
+        { // Move Camera Out or Zoom Out
+            zoom = true;
+            offset.z = offset.z - 5.0f;
+        }
         transform.position = player.transform.position + offset;
     }
 }
