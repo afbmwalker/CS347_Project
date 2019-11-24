101d63043cfe4871d4a41f08211457e3e5a9b7da
John <jfm0015@uah.edu>
2019-11-24 16:14:55 -0600

    added turkey kill and game ending script
    also includes a dead turkey prefab. The main turkey was accidently changed and so reinstantiated from prefab

---
+++ CS347 Major Project/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameEnding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject triggeringObj;
+        triggeringObj = other.gameObject;
+        if (triggeringObj.tag == "DeadTurkey")
+        {
+            GameObject GlobalPrompt;                    //This block gets the reference
+            Text TextObj;
+            GlobalPrompt = GameObject.Find("GlobalPrompt");            //to the text storing the score,
+            TextObj = GlobalPrompt.GetComponent<Text>();      //and sets scoreText to this
+            TextObj.text = "WINNER WINNER TURKEY DINNER!";
+            Invoke("QuitApplication", 4.0f);
+        }
+    }
+    private void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
---
+++ CS347 Major Project/Assets/Scripts/TurkeyKill.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurkeyKill : MonoBehaviour
+{
+    public GameObject DeadTurkey;
+    public uint health;
+    // Start is called before the first frame update
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collidedObj = collision.gameObject;
+        if (collidedObj.tag == "Weapon")
+        {
+            health--;
+            if (health==0)
+            {
+                Instantiate<GameObject>(DeadTurkey);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
