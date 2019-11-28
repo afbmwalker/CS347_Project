9be3bbb859938d6cea584f470fab1423e8e7b56d
ddc0021 <57074718+ddc0021@users.noreply.github.com>
2019-11-27 21:37:00 -0600

    Added money counter that appears upon entering the store and disappears upon exiting.

---
+++ CS347 Major Project/Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyScript : MonoBehaviour
+{
+	public Text my_money;
+	public string amount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+	{
+		GameObject collidedobject;
+		collidedobject = collision.gameObject;
+
+        if(collidedobject.gameObject.tag == "Store")
+		{
+			GameObject dollars;
+			dollars = GameObject.Find("MyMoney");
+			my_money = dollars.GetComponent<Text>();
+			my_money.text = "$25";
+			amount = my_money.text;
+		}
+
+        if(collidedobject.gameObject.tag == "Sidewalk")
+		{
+			GameObject dollars;
+			dollars = GameObject.Find("MyMoney");
+			my_money = dollars.GetComponent<Text>();
+			my_money.text = "";
+			amount = my_money.text;
+		}
+	}
+}
