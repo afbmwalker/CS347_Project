5ec528d373f9d04c8d168fca8f948d39ea54a769
John <jfm0015@uah.edu>
2019-11-29 16:07:32 -0600

    furniture destruction+ some prefabs

--- CS347 Major Project/Assets/Scripts/FurnitureDestruction.cs
+++ CS347 Major Project/Assets/Scripts/FurnitureDestruction.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FurnitureDestruction : MonoBehaviour
 {
+
+    public float forceMagnitude;
+    public float upwardMag;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +18,23 @@ public class FurnitureDestruction : MonoBehaviour
     {
         
     }
-
+    
     private void OnCollisionEnter(Collision collision)
 	{
-
+    
+        GameObject collidedObj;
+        Rigidbody collidedRb;
+        Vector3 impulseVect;
+        
+        collidedObj = collision.gameObject;
+        collidedRb = collidedObj.GetComponent<Rigidbody>();
+        if (collidedRb != null)
+        {
+            impulseVect = collidedObj.transform.position - this.transform.position;
+            impulseVect.Normalize();
+            impulseVect *= forceMagnitude;
+            impulseVect.y = upwardMag; 
+            collidedRb.AddForce(impulseVect, ForceMode.Impulse);
+        }
 	}
 }
