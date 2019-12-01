0dab4ec570d69531aea1245ae36fa774cc5bf32b
Brendan Walker <afbmwalker@gmail.com>
2019-11-30 20:14:44 -0600

    Added Axe Attack

--- CS347 Major Project/Assets/Scripts/AxeScript.cs
+++ CS347 Major Project/Assets/Scripts/AxeScript.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 
 public class AxeScript : MonoBehaviour
 {
-    public float attackSpeed = 1.0f;
+    public GameObject target;
+    public float attackSpeed = 0.1f;
     private bool attackDelay = false;
+    private Quaternion idleRotation;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   // initialize the idle rotation value to return to default
+        idleRotation = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   // If attack input given, attack
         if(attackDelay == false && Input.GetKey("space"))
         {
-            attackDelay = true;
-            Invoke("Attack", attackSpeed);
-
+            attackDelay = true; // toggle bool to prevent attack spamming
+            Invoke("AttackReset", attackSpeed); // reset bool after attack delay
+            transform.rotation = target.transform.rotation; // rotate axe to preset gameobject rotation
         }
     }
 
     // Creates a delay between attacks
-    void Attack()
-    {
+    void AttackReset()
+    {   // reset attack delay
         attackDelay = false;
+        // reset Axe to default position/rotation
+        transform.rotation = idleRotation;
     }
 }
