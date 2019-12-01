514efcfa045b9054f3bda0929a1423ad5c20c80b
Brendan Walker <afbmwalker@gmail.com>
2019-11-30 20:58:55 -0600

    Added Weapon Pickup In Store Only

--- CS347 Major Project/Assets/Scripts/WeaponScript.cs
+++ CS347 Major Project/Assets/Scripts/WeaponScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponScript : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class WeaponScript : MonoBehaviour
     public GameObject axe;
     public GameObject gun;
     public GameObject sling;
+    public GameObject weaponText;
+    private bool inStore = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,27 +20,42 @@ public class WeaponScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(weaponSelected == false && Input.GetKey("1"))
+        if(inStore == true && weaponSelected == false && Input.GetKey("1"))
         {
             axe.SetActive(true);
             weaponSelected = true;
         }
-        if (weaponSelected == false && Input.GetKey("2"))
+        if (inStore == true && weaponSelected == false && Input.GetKey("2"))
         {
             gun.SetActive(true);
             weaponSelected = true;
         }
-        if (weaponSelected == false && Input.GetKey("3"))
+        if (inStore == true && weaponSelected == false && Input.GetKey("3"))
         {
             sling.SetActive(true);
             weaponSelected = true;
         }
-        if (weaponSelected == true && Input.GetKey("4"))
+        if (inStore == true && weaponSelected == true && Input.GetKey("4"))
         {
             gun.SetActive(false);
             sling.SetActive(false);
             axe.SetActive(false);
             weaponSelected = false;
+        }
+    }
+
+    // check to see if in store
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Store")
+        {
+            weaponText.SetActive(true);
+            inStore = true;
+        }
+        if(other.gameObject.tag == "Sidewalk")
+        {
+            weaponText.SetActive(false);
+            inStore = false;
         }
     }
 }
